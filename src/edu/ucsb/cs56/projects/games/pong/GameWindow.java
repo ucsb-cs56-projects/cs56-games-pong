847e3a0e89d1db829f5cc60228ba9f4f7845ddab
Angel Ortega <an22ortega@gmail.com>
2016-02-22 00:02:20 -0800

    AO - GameWindow class fixture in the works

---
+++ src/edu/ucsb/cs56/projects/games/pong/GameWindow.java
@@ -0,0 +1,33 @@
+package edu.ucsb.cs56.projects.games.pong;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by angel on 2/21/16.
+ */
+public class GameWindow extends JFrame {
+    private static final Dimension DIMENSION = new Dimension(640,480);
+    static Screen screen;
+
+
+    public GameWindow(String title, Dimension dimension) {
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(dimension);
+        this.setLocationRelativeTo( null );
+        this.setBackground(Color.BLACK);
+    }
+
+    public static void main(String[] args) {
+        GameWindow window = new GameWindow("Main Menu",DIMENSION);
+//        MainMenuUI gameMenu = new MainMenuUI(DIMENSION);
+//        window.add(gameMenu);
+        window.setVisible(true);
+        call();
+    }
+    public static void call() {
+        screen = new Screen(WIDTH,HEIGHT);
+    }
+
+}
