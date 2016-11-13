7261ed5fa7145f7f991e137f14dd93a166d1bdfb
Zhancheng Qian <zhanchengqian@gmail.com>
2016-11-12 21:36:30 -0800

    Revert "Fixed issues #34 and #59"

--- src/edu/ucsb/cs56/projects/games/pong/menu/DifficultyLevelPrompt.java
+++ src/edu/ucsb/cs56/projects/games/pong/menu/DifficultyLevelPrompt.java
@@ -22,14 +22,11 @@ public class DifficultyLevelPrompt extends JPanel {
         put("Chaos",170);
 
     }};
-    public Map<String, Integer> getHashmap(){
-	return DIFFICULTIES;}
     public DifficultyLevelPrompt() {
-	
         JComboBox selections = new JComboBox(DIFFICULTIES.keySet().toArray());
         this.add(selections);
         int responseFromUser = JOptionPane.showConfirmDialog(null,this,
-                "Select your difficulty:",JOptionPane.OK_CANCEL_OPTION,JOptionPane.OK_OPTION);
+                "Selection your difficulty:",JOptionPane.OK_CANCEL_OPTION,JOptionPane.OK_OPTION);
         Object itemChosen = selections.getSelectedItem();
         if (responseFromUser == JOptionPane.OK_OPTION && itemChosen != null) {
             difficulty = DIFFICULTIES.get(itemChosen.toString()).intValue();
@@ -38,6 +35,7 @@ public class DifficultyLevelPrompt extends JPanel {
             this.remove(this);
         }
     }
+
     public boolean isSelected() {
         return selected;
     }
--- src/edu/ucsb/cs56/projects/games/pong/menu/PlayTextComponent.java
+++ src/edu/ucsb/cs56/projects/games/pong/menu/PlayTextComponent.java
@@ -1,6 +1,5 @@
 package edu.ucsb.cs56.projects.games.pong.menu;
 
-import edu.ucsb.cs56.projects.games.pong.gameplay.DifficultyLevel;
 import edu.ucsb.cs56.projects.games.pong.gameplay.Screen;
 import sun.audio.AudioPlayer;
 import sun.audio.AudioStream;
@@ -17,13 +16,10 @@ import java.io.InputStream;
  */
 public class PlayTextComponent extends MenuTextComponent  {
     private Screen screen;
-    public static int newwidth;//
-    public static int newheight;//
-    public static int screenfactor;
-    public static int difficulty;
     public PlayTextComponent(String title, Color backgroundColor) {
         super(title, backgroundColor);
     }
+
     @Override
     public void mouseClicked(MouseEvent mouseEvent) {
         if(textColor.equals(Color.BLUE)) {
@@ -31,17 +27,8 @@ public class PlayTextComponent extends MenuTextComponent  {
             DifficultyLevelPrompt prompt = new DifficultyLevelPrompt();
             if(prompt.isSelected()) {
                 playGameStartAudio();
-		DifficultyLevel d=new DifficultyLevel(prompt.getDifficulty());
-		newwidth=d.getNewWidth();
-		//System.out.println("newwidth in play text is " + newwidth);
-		newheight=d.getNewHeight();
-		//System.out.println("newheight in play text is " + newheight);
-		screenfactor=d.getScreenFactor();
-		//System.out.println("newheight in play text is " + screenfactor);
-		difficulty=d.getDifficulty();
-		//System.out.println("difficulty in play text is " + screenfactor);
-                screen = new Screen(8* prompt.getDifficulty(), 6 * prompt.getDifficulty());
-	    }
+                screen = new Screen(8 * prompt.getDifficulty(), 6 * prompt.getDifficulty());
+            }
         }
 //        else if(textColor.equals(Color.BLUE) && screen != null) {
 //            DifficultyLevelPrompt prompt = new DifficultyLevelPrompt();
@@ -51,15 +38,17 @@ public class PlayTextComponent extends MenuTextComponent  {
 //            }
 //        }
     }
-    
+
     @Override
     public void mousePressed(MouseEvent mouseEvent) {
+
     }
 
     @Override
     public void mouseReleased(MouseEvent mouseEvent) {
+
     }
-    
+
     @Override
     protected void playGameStartAudio() {
         try {
