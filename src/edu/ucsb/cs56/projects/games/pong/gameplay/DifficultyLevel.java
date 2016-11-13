3380e97677e7d18b8f1fd8acfdeb0d95a0460a37
Alexander Ngo <alexanderngo@umail.ucsb.edu>
2016-11-13 15:25:41 -0800

    AN/MB merge

--- src/edu/ucsb/cs56/projects/games/pong/gameplay/DifficultyLevel.java
+++ src/edu/ucsb/cs56/projects/games/pong/gameplay/DifficultyLevel.java
@@ -3,9 +3,9 @@ package edu.ucsb.cs56.projects.games.pong.gameplay;
 import java.awt.Graphics;
 import edu.ucsb.cs56.projects.games.pong.menu.PlayTextComponent;
 import java.io.*;
-/** edu.ucsb.cs56.projects.games.pong.gameplay.Difficultylevel is the class that holds the diffulty and other parameters associated with varying difficulty levels
+/** edu.ucsb.cs56.projects.games.pong.gameplay.Difficultylevel is the class that holds the diffculty and other parameters associated with varying difficulty levels
  @author Millan Batra, Alex Ngo
- @version CS56, Winter 2016, UCSB
+ @version CS56, Fall 2016, UCSB
 */
 
 public class DifficultyLevel
