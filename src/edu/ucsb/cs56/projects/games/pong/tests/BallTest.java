6d4ff7bd44bfca148dedad22f573c9516157de9b
Xingxing Geng <xingxinggeng@umail.ucsb.edu>
2018-02-28 21:41:46 -0800

    Added JUnits

--- src/edu/ucsb/cs56/projects/games/pong/tests/BallTest.java
+++ src/edu/ucsb/cs56/projects/games/pong/tests/BallTest.java
@@ -1,7 +1,7 @@
 package edu.ucsb.cs56.projects.games.pong.tests;
 
 import edu.ucsb.cs56.projects.games.pong.gameplay.Ball;
-
+import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -9,65 +9,88 @@ import static org.junit.Assert.assertEquals;
  *
  * @author Timothy Fok
  * @version CS56, Spring 2012, cp1 05.18.12
+ * @author Samuel Fu, Xingxing Geng
+ * @version CS56, Winter 2018
  * @see Ball
  */
 
 public class BallTest 
 {
     /**Default BallTest constructor*/
-    public BallTest() {}
+    public BallTest(){}
     
-    /**
-       test constructor from BallTest
-       //@see Ball
-    */
-    /*  
-    @Test public void testConstructor()
+    @Test public void testConstructor_1()
     {
-        Ball ball = new Ball(20,20,20,20);
-        assertEquals(20,ball.getXpos());
-  assertEquals(20,ball.getYpos()); 
-  }*/
-
-    /**
-       test constructor again from BallTest
-     //  @see Ball
-    */
-   /*
-    @Test public void testConstructor2()
+        Ball ball = new Ball(20,20,20,20,false);
+        assertEquals(20,ball.getWidth());
+        assertEquals(20,ball.getHeight());
+  }
+    
+    @Test public void testConstructor_2()
     {
-        Ball ball = new Ball(30,100,20,20);
-        assertEquals(30,ball.getXpos());
-  assertEquals(100,ball.getYpos()); 
-    }
-   */
-
-    /**
-       test getdx
-     //  @see Ball#getdx
-    */
-    /*
-    @Test public void testgetdx()
-    {
-        Ball ball = new Ball(30,100,20,20);
-  ball.setdx(5);
-        assertEquals(5,ball.getdx());
+        Ball ball = new Ball(0,0,30,30,true);
+        assertEquals(0,ball.getXCoordinate());
+        assertEquals(0,ball.getYCoordinate());
     }
 
-    */
-    /**
-       test getdy
-      // @see Ball#getdy
-    */  
-    /*
-    @Test public void testgetdy()
+    @Test public void testConstructor_4()
     {
-        Ball ball = new Ball(30,100,20,20);
-  ball.setdy(7);
-        assertEquals(7,ball.getdy());
-
+        Ball ball = new Ball(0,0,30,30,true);
+        assertEquals(-1,ball.getSpeed());
     }
-    */
+    
+    @Test public void testConstructor_3()
+    {
+        Ball ball = new Ball(0,0,30,30,true);
+        assertEquals(-1,ball.getSpeed());
+    }
+    
+    private Ball b1 =  new Ball(0,0,30,30,false);
+    private Ball b2 =  new Ball(200,200,30,30,false);
+    private Ball b3 =  new Ball(100,50,20,20,true);
+    private Ball b4 =  new Ball(50,100,20,30,true);
+    
+    @Test public void testgetYVelocity()
+    {
+        b1.setYVelocity(5);
+        b2.setYVelocity(10);
+        assertEquals(5,b1.getYVelocity());
+        assertEquals(10,b2.getYVelocity());
+    }
+    
+    @Test public void testisAttached()
+    {
+        b1.setAttached(false);
+        b2.setAttached(true);
+        assertEquals(false,b1.isAttached());
+        assertEquals(true,b2.isAttached());
+    }
+    
+    @Test public void testgetSpeed()
+    {
+        b1.setSpeed(5);
+        b2.setSpeed(10);
+        assertEquals(5,b1.getSpeed());
+        assertEquals(10,b2.getSpeed());
+    }
+    
+    @Test public void testgetBallLost()
+    {
+        b1.setBallsLost(2);
+        b2.setBallsLost(1);
+        assertEquals(2,b1.getBallsLost());
+        assertEquals(1,b2.getBallsLost());
+    }
+    
+    @Test public void testBallAttachUpdate()
+    {
+        b1.setAttached(true);
+        b2.setAttached(true);
+        b1.ballAttachUpdate();
+        b2.ballAttachUpdate();
+        assertEquals(0,b1.getYVelocity());
+        assertEquals(0,b2.getYVelocity());
+    }
 
 
 }
--- src/edu/ucsb/cs56/projects/games/pong/tests/PongTest.java
+++ src/edu/ucsb/cs56/projects/games/pong/tests/PongTest.java
@@ -1,7 +1,9 @@
 package edu.ucsb.cs56.projects.games.pong.tests;
 
 import edu.ucsb.cs56.projects.games.pong.gameplay.Pong;
-
+import edu.ucsb.cs56.projects.games.pong.gameplay.Ball;
+import edu.ucsb.cs56.projects.games.pong.gameplay.Paddle;
+import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -9,6 +11,8 @@ import static org.junit.Assert.assertEquals;
  *
  * @author Jake Dumont, Heneli Kailahi
  * @version CS56, Spring 2013, CS56-Games-Pong
+ * @author Samuel Fu, Xingxing Geng
+ * @version CS56, Winter 2018
  * @see Pong
  */
 public class PongTest 
@@ -20,35 +24,74 @@ public class PongTest
      * test constructor from Pong
      * @see Pong
      */
-  /*@Test 
+    private Paddle p1 = new Paddle(30,30,50,2);
+    private Paddle p2 = new Paddle(15,15,30,3);
+    private Paddle p3 = new Paddle(40,40,90,4);
+    private Paddle p4 = new Paddle(25,25,50,5);
+    private Paddle p5 = new Paddle(30,30,100,0,false);
+    
+    private Ball b1 =  new Ball(0,0,30,30,false);
+    private Ball b2 =  new Ball(200,200,30,30,false);
+    private Ball b3 =  new Ball(100,50,20,20,true);
+    private Ball b4 =  new Ball(50,100,20,30,true);
+    
+  @Test
     public void testConstructor()
     {
       Pong game = new Pong();
-      Paddle pp = new Paddle(8, 160);
-      Ball bb = new Ball(300, 300, 20, 20);
+      Paddle pp = new Paddle(30,30,50,2);
+      Ball bb = new Ball(0,0,30,30,false);
       
-      assertEquals(pp.getXpos(),game.p1.getXpos());
-      assertEquals(pp.getYpos(),game.p1.getYpos());
-      
-      assertEquals(bb.getXpos(), game.b.getXpos());
-      assertEquals(bb.getYpos(), game.b.getYpos());
-    }*/
+      assertEquals(pp.getXCoordinate(),30);
+      assertEquals(pp.getYCoordinate(),30);
+    }
 
+    @Test
+    public void testgetHits()
+    {
+        Pong game = new Pong();
+        game.hitsReset();
+        assertEquals(0,game.getHits());
+    }
     
-    //////////////////////////////////////////////
-    // tests the incrementor
-    // for loop
-    ////////////////////////////////   
-    /**
-     * test getPoints
-     * @see Pong#getPoints
-     */
-    /*
-      @Test public void testgetHits()
-      {
-      Pong game = new Pong();
-      game.setPoints(5);
-      assertEquals(5,game.getPoints());
-      }
-    */
+    @Test
+    public void testhitReset()
+    {
+        Pong game = new Pong();
+        game.hitsReset();
+        
+        assertEquals(0,game.getHits());
+    }
+    
+    @Test
+    public void testincrementHits()
+    {
+        Pong game = new Pong();
+        game.hitsReset();
+        game.incrementHits();
+        assertEquals(1,game.getHits());
+    }
+    
+    @Test
+    public void testsetwinner()
+    {
+        Pong game = new Pong();
+        game.setWinner(p1);
+        assertEquals(p1,game.getWinner());
+    }
+    
+    @Test
+    public void testgetPlayer1()
+    {
+        Pong game = new Pong();
+        assertEquals(p1,game.getPlayer1());
+    }
+    
+    @Test
+    public void testgetPlayer2()
+    {
+        Pong game = new Pong();
+        assertEquals(p2,game.getPlayer2());
+    }
+    
 }
