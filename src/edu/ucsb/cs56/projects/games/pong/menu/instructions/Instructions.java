24052172128a1900478cdda4ff01095516dade96
Xingxing Geng <xingxinggeng@cs.ucsb.edu>
2018-03-14 15:16:10 -0700

    delete instruction

--- src/edu/ucsb/cs56/projects/games/pong/menu/instructions/Instructions.java
+++ src/edu/ucsb/cs56/projects/games/pong/menu/instructions/Instructions.java
@@ -3,7 +3,6 @@ package edu.ucsb.cs56.projects.games.pong.menu.instructions;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.*;
 
 /** edu.ucsb.cs56.projects.games.pong.menu.instructions.Instructions is the classs that displays the instructions to the game when the user presses the instructions button
  @author Vincent Gandolfo, Krishna Lingampalli
@@ -16,7 +15,7 @@ public class Instructions{
     JFrame jf;
 
     /** Constructor that sets up the JFrame and Components that allow the instructions to be displayed */
-    public Instructions()throws Exception
+    public Instructions()
     {
 	setUpFrame();
 	setUpFrameComponents();
@@ -36,7 +35,7 @@ public class Instructions{
     }
 
     /** setUpFrameComponents()  sets up the layout on the frame and puts on the Instructions and the "Back To Main Menu" button */
-    public void setUpFrameComponents()throws Exception
+    public void setUpFrameComponents()
     {
 	JButton backToMain = new JButton( "Back To Main Menu" );
 	backToMain.addActionListener( new BackToMainListener() );
@@ -58,15 +57,10 @@ public class Instructions{
     /** displayInstructions() displays the Instructions on how to play
      * @return String returns instructions
      */
-    public String displayInstructions()throws Exception
+    public String displayInstructions()
     {
-	File file = new File("instructions.txt");
-        BufferedReader br = new BufferedReader(new FileReader(file));
-
-        String st = br.readLine();
-	return ("" + st);
-/*	return ("<html><p><br>Difficulty:<br>The Screen size increases with the difficulty that is chosen.<br><br>How To Play:<br><br>Player 1 uses W to move the paddle up,<br> S to move their paddle down, and A to grab the ball.<br>Player 2 uses the Up and Down arrows to move their paddle up and<br> down and the left arrow to grab the ball.<br><br>When ball is being held and the player releases it while moving their paddle up or down,<br>ball will be released with the same speed that the paddle is moving<br><br>Hit the space bar to move the ball initially.<br><br>Press P to pause the game.<br>Press M to return to the Main Menu.<br>You lose a life if you miss the ball.<br>When you lose a life, the total hits will be added to your opponents score.<br>The winner is the player with remaining lives.<br><br>Chaos Mode:<br> very small ball on large screen moving at faster than normal speed<br><br>Custom Mode:<br>Allows players to select both level and how many balls they would like<br>to be in play at one time at the level selected.<br><br>Information on Ball to Paddle Speed:<br>When ball is released, it has the same y-velocity as your paddle.<br>The ball increases in speed after each hit of paddle.</p></html>");
- */   }
+	return ("<html><p><br>Difficulty:<br>The Screen size increases with the difficulty that is chosen.<br><br>How To Play:<br><br>Player 1 uses W to move the paddle up,<br> S to move their paddle down, and A to grab the ball.<br>Player 2 uses the Up and Down arrows to move their paddle up and<br> down and the left arrow to grab the ball.<br><br>When ball is being held and the player releases it while moving their paddle up or down,<br>ball will be released with the same speed that the paddle is moving<br><br>Hit the space bar to move the ball initially.<br><br>Press P to pause the game.<br>Press M to return to the Main Menu.<br>You lose a life if you miss the ball.<br>When you lose a life, the total hits will be added to your opponents score.<br>The winner is the player with remaining lives.<br><br>Chaos Mode:<br> very small ball on large screen moving at faster than normal speed<br><br>Custom Mode:<br>Allows players to select both level and how many balls they would like<br>to be in play at one time at the level selected.<br><br>Information on Ball to Paddle Speed:<br>When ball is released, it has the same y-velocity as your paddle.<br>The ball increases in speed after each hit of paddle.</p></html>");
+    }
 
     /** The action when the button "Back To Main Menu" is pressed. It just makes the frame not visible, so it appears to be closed and does not need to create a new instance of Instructions for every time the user clicks how to play */
     class BackToMainListener implements ActionListener{
