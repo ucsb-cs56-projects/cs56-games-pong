8b888db72e4b1dbbd4d1450870b729c6c6df4cb7
Samuel Fu <jiefu@umail.ucsb.edu>
2018-03-14 17:06:06 -0700

    start

--- src/edu/ucsb/cs56/projects/games/pong/gameplay/Screen.java
+++ src/edu/ucsb/cs56/projects/games/pong/gameplay/Screen.java
@@ -120,7 +120,7 @@ public class Screen{
 	    }
 	    
 	    //If all the balls have stopped then pause the game
-	    if(game.checkBallStopped()) {
+	    if(Pong.isPaused) {
 		g.drawString( "Game Paused", Screen.w/2 - 100, Screen.h/2 - 100 );
 		g.drawString( "Press M to return to Main Menu", Screen.w/2 - 220, Screen.h/2 + 100 );
 	    }
